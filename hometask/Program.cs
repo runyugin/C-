073b809538f0ada083e99a85b21bee9105d74e70
krasnoperov <runyugin@mail.ru>
2023-03-04 15:16:09 +0300

    hometask_2_1_done

--- hometask/Program.cs
+++ hometask/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+String e = Console.ReadLine();
+
+Console.WriteLine($"{e} -> {e[1]}");
+
+
+
