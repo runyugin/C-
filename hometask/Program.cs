f4a5253568bb1b8c50eee3d539b27858838ace59
krasnoperov <runyugin@mail.ru>
2023-03-09 15:45:37 +0300

    hometask_3_1_done

--- hometask/Program.cs
+++ hometask/Program.cs
@@ -1,2 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1
+
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом. Проверка без применения строк
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+
+
+using System;
+
+int num = Int32.Parse(Console.ReadLine());
+
+int[] number = toIntegerReversArray(num);
+
+
+
+Console.WriteLine($"{num} -> {IsPolindrom(number)}");
+
+
+int[] toIntegerReversArray (int number)
+    {
+        int a = number;
+
+        List<int> l = new List<int>();
+
+        while(a>0)
+        {
+                l.Add(a % 10);
+                a = a / 10; 
+        }
+        return l.ToArray();
+    }
+
+bool IsPolindrom(int[] array){
+
+    for (int i = 0; i < array.Length; i++)
+    {
+
+        if(array[i]!=array[array.Length-i-1]){
+            return false;
+        }
+    }
+
+    return true;
+}
