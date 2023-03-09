db85eaec082e848cb183563b19a85323683d8b71
krasnoperov <runyugin@mail.ru>
2023-03-09 16:37:31 +0300

    homework_3_3_done

--- hometask/Program.cs
+++ hometask/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 3
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+
+long num = long.Parse(Console.ReadLine());
+
+
+Console.WriteLine($"{num} -> {string.Join(", ", numCube(num))}");
+
+
+long[] numCube(long num){
+
+    long[] arr = new long[num];
+
+for (int i = 1; i <= num; i++)
+{   
+    arr[i-1] = (long)Math.Pow(i, 3);
+}
+
+return arr;
+
+}
