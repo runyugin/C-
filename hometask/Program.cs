fa33678feafa10bebe43a73e0be6a9be580eca1e
krasnoperov <runyugin@mail.ru>
2023-03-13 17:08:24 +0300

    homework_5_3_done

--- hometask/Program.cs
+++ hometask/Program.cs
@@ -1,1 +1,61 @@
-﻿// 
+﻿// Задача 3: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+
+
+    int[] array = arrayRandom();
+
+    int different = maxNum(array)-minNum(array);
+
+    Console.WriteLine($"[{string.Join(", ", array)}] -> {different}");
+
+
+int[] arrayRandom(){
+
+    int[] result = new int[8];
+
+    for (int i = 0; i < result.Length; i++) {  
+    result[i] = new Random().Next(-100,100);
+    }
+
+    return result;
+
+}
+
+int minNum(int[] arr){
+
+
+    int result = int.MaxValue;
+
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+
+            if(result>arr[i]){
+                result=arr[i];
+            }
+
+        }
+
+    return result;
+}
+
+
+int maxNum(int[] arr){
+
+
+    int result = int.MinValue;
+
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+
+            if(result<arr[i]){
+                result=arr[i];
+            }
+
+        }
+
+    return result;
+}
