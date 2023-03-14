471aafaf82ec9f8e7a9a15d0f3246db83f575886
krasnoperov <runyugin@mail.ru>
2023-03-14 16:28:11 +0300

    homework_7_1_done

--- hometask/Program.cs
+++ hometask/Program.cs
@@ -1,1 +1,31 @@
-﻿// 
+﻿// Задача 1. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+int m=3;
+int n=4;
+
+double[,] arr = new double[m,n];
+
+Random x = new Random(); 
+
+
+        Console.WriteLine($"m = {m}, n = {n}");
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        arr[i,j] = Convert.ToDouble(x.Next(-100, 100)/10.0);;
+        Console.Write($"{arr[i,j]}, ");
+    }
+
+        Console.WriteLine();
+}
+
